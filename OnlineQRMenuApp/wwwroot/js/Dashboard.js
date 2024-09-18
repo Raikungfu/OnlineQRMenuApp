5e1a31e758fe7482ed7eb461c8695280ba21e4d0
Raikungfu <raikungfu@gmail.com>
2024-09-19 04:30:50 +0700

    update

--- OnlineQRMenuApp/wwwroot/js/Dashboard.js
+++ OnlineQRMenuApp/wwwroot/js/Dashboard.js
@@ -1,18 +1,22 @@
 ﻿const toggleMenuOpenButton = document.getElementById('toggle-menu-open');
 const toggleMenuCloseButton = document.getElementById('toggle-menu-close');
-const toggleMenuButton = document.getElementById('toggle-menu');
 const sidebar = document.querySelector('.sidebar');
 const mainNavMenu = document.querySelector('.main-nav-menu');
 
-toggleMenuOpenButton.addEventListener('click', () => {
-    sidebar.classList.toggle('show');
-    mainNavMenu.classList.toggle('expanded');
-});
+function toggleMenu() {
+    if (sidebar && mainNavMenu) {
+        sidebar.classList.toggle('show');
+        mainNavMenu.classList.toggle('expanded');
+    }
+}
 
-toggleMenuCloseButton.addEventListener('click', () => {
-    sidebar.classList.toggle('show');
-    mainNavMenu.classList.toggle('expanded');
-});
+if (toggleMenuOpenButton) {
+    toggleMenuOpenButton.addEventListener('click', toggleMenu);
+}
+
+if (toggleMenuCloseButton) {
+    toggleMenuCloseButton.addEventListener('click', toggleMenu);
+}
 
 document.addEventListener('DOMContentLoaded', function () {
     const mobileNav = document.getElementById('main-mobile-nav');
